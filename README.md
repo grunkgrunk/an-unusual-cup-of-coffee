0830edad94e1d2fb4074a1b83e3213beecf61ae7
grunkgrunk <danielgrunkin@gmail.com>
2018-07-29 23:26:43 +0200

    better instructions

---
+++ README.md
@@ -0,0 +1,3 @@
+# An Unusual Cup of Coffee
+
+A game about life as a barista. Made for the [COFFEE JAM](https://itch.io/jam/coffee-jam). Music by [Matthew Pablo](https://opengameart.org/content/trouble-makers-coolriff-jazz)
--- gui.gd
+++ gui.gd
@@ -6,6 +6,39 @@ var invis = Color(1, 1, 1, 0)
 var vis = Color(1, 1, 1, 1)
 var begin = true
 
+var knows_controls = false
+
+var up_pressed = false
+var down_pressed = false
+var left_pressed = false
+var right_pressed = false
+var grab_pressed = false
+var touch_pressed = false
+
+func _input(event):
+	if knows_controls:
+		return
+	if event.is_action_pressed("ui_up"):
+		up_pressed = true
+	if event.is_action_pressed("ui_down"):
+		down_pressed = true
+	if event.is_action_pressed("ui_left"):
+		left_pressed = true
+	if event.is_action_pressed("ui_right"):
+		right_pressed = true
+	if event.is_action_pressed("grab"):
+		grab_pressed = true
+	if event.is_action_pressed("touch"):
+		touch_pressed = true
+	
+	var arrows = up_pressed and down_pressed and left_pressed and right_pressed
+	var actions = grab_pressed and touch_pressed
+	knows_controls = arrows and actions
+	if knows_controls:
+		tween.interpolate_property($controls, "modulate",vis, invis, 1,Tween.TRANS_CUBIC, Tween.EASE_IN_OUT, 2)
+		$tween.start()
+		
+
 func _ready():
 	$heading.modulate = invis
 	$controls.modulate = invis
@@ -19,7 +52,6 @@ func _ready():
 	
 	tween.interpolate_property($heading, "modulate",vis, invis, 1,Tween.TRANS_CUBIC, Tween.EASE_IN_OUT, 6)
 	tween.interpolate_property($by, "modulate",vis, invis, 1,Tween.TRANS_CUBIC, Tween.EASE_IN_OUT, 6)
-	tween.interpolate_property($controls, "modulate",vis, invis, 1,Tween.TRANS_CUBIC, Tween.EASE_IN_OUT, 6)
 	tween.start()
 
 func _on_hand_quest_game_over():
