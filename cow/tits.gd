6d35a0151ef8f7968b0ab0a6b7ba824087885535
grunkgrunk <danielgrunkin@gmail.com>
2018-08-05 20:13:06 +0200

    fixed milk bug

--- cow/tits.gd
+++ cow/tits.gd
@@ -1,19 +1,18 @@
 extends Area2D
 
 func _ready():
-	monitoring = false
 	hide()
 
 func interact(hand):
 	pass
 	
 func _on_begin_touch(hand, obj):
-	if obj == self:
+	if obj == self and visible:
 		for s in $sources.get_children():
 			s.start_milk()
 
 func _on_tits_mounted():
-	monitoring = true
 	show()
+	add_to_group("flesh")
 	$moo.play()
 	
